91925ea3b243d831bfff05961fd57bfff4c970ac
Pradip <dellpidarp57@gmail.com>
2024-04-25 11:15:09 +0545

    Error handler middleware created in express app

--- src/config/express.config.js
+++ src/config/express.config.js
@@ -3,5 +3,20 @@ const express = require('express')
 const app = express()
 
 
+app.use((error,req,res,next)=>{
+
+    const codeStatus = error.code || 500
+    const data = error.data || null
+    const message = error.message || "Internal server error"
+
+    res.status(codeStatus).json({
+
+        result:data,
+        message:message,
+        meta:null
+    })
+
+
+})
 
 module.exports = app
