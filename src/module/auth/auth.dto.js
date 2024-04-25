3d409014a672ac346dc3121e50b2a1679d3cf85f
Pradip <dellpidarp57@gmail.com>
2024-04-25 14:35:16 +0545

    validation process with the help of joi package, dto established

---
+++ src/module/auth/auth.dto.js
@@ -0,0 +1,10 @@
+const Joi = require("joi");
+
+const registerDTO = Joi.object({
+  name: Joi.string().min(4).max(9).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+  confirmPassword: string().valid(Joi.ref("password")).required(),
+  role: Joi.string().pattern(/^(staff|customer)$/).required()
+});
+module.exports = { registerDTO };
