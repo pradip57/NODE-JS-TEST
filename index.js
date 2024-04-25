5336321c5a495731bee3367459314044264b8d25
Pradip <dellpidarp57@gmail.com>
2024-04-25 11:09:58 +0545

    express app connected in server

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 require('dotenv').config()
 const http = require('http')
+const app = require('./src/config/express.config')
 
-const server = http.createServer()
+const server = http.createServer(app)
 
 
 server.listen(process.env.API_PORT,'127.0.0.1',(err)=>{
