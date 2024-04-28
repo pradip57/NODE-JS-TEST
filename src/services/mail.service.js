9180c281b2f59937a68d92f80371c777d2845f0d
Pradip <dellpidarp57@gmail.com>
2024-04-28 16:08:44 +0545

    send Email func defined and configured with sendMail method

--- src/services/mail.service.js
+++ src/services/mail.service.js
@@ -11,15 +11,27 @@ class MailServices {
         auth: {
           user: SMTP_USERNAME,
           pass: SMTP_PASSWORD,
-        }
+        },
       });
     } catch (exception) {
       console.log(exception);
       throw new Error("Error connecting email services");
     }
   }
-
-  
+  sendEmail = async (to, subject, message, attachments = null) => {
+    try {
+      const mailStatus = await this.transport.sendMail({
+        to: to,
+        from: SMTP_FROM,
+        subject: subject,
+        html: message,
+        attachments: attachments,
+      });
+    } catch (exception) {
+      console.log(exception);
+      throw new Error("Error sending mail");
+    }
+  };
 }
 
 const mailServ = new MailServices();
