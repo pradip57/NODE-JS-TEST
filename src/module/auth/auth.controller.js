8e581fb063f27de4843ad2f42dfee2cd1289c36f
Pradip <dellpidarp57@gmail.com>
2024-04-28 16:44:57 +0545

    send mail function called

--- src/module/auth/auth.controller.js
+++ src/module/auth/auth.controller.js
@@ -1,31 +1,49 @@
-const bcrypt = require('bcryptjs')
+require('dotenv').config()
+const bcrypt = require("bcryptjs");
+const mailServ = require("../../services/mail.service");
 
 class AuthController {
+  register = async (req, res, next) => {
+    try {
+      const payload = req.body;
 
- register = (req,res,next) => {
+      payload.password = bcrypt.hashSync(payload.password, 10);
+      payload.status = "inactive";
 
-    try{
+      if (req.file) {
+        payload.file = req.file.filename;
+      }
 
-        const payload = req.body
+      const registeredData = {
+        ...payload,
+        _id: "123",
+      };
 
-        payload.password = bcrypt.hashSync(payload.password,10)
-        payload.status = "inactive"
+      await mailServ.sendEmail(
+        registeredData.email,
+        "Activate your account",
+        `Dear ${registeredData.name} <br/>
+        <p>You have registered your account with username <strong>${registeredData.email}</strong></p>
+        <p>Please click the link below or copy and paste in browser to activate your account</p>
+        <a href = "${process.env.FRONTEND_URL}/activate/${registeredData.activationToken}">
+        ${process.env.FRONTEND_URL}/activate/${registeredData.activationToken}
+        </a><br/>
+        <p>Regards,</p>
+        <p>${process.env.SMTP_FROM}</p>
+        <p><small><em>Please do not reply to this email via any mail service.</em></small></p>
+        `
+      );
 
-
-        res.json({
-            result:payload,
-            message:"Register Success",
-            meta:null
-        })
-
-    }catch(exception){
-        next(exception)
+      res.json({
+        result: payload,
+        message: "Register Success",
+        meta: null,
+      });
+    } catch (exception) {
+      next(exception);
     }
-
- }   
-
-
+  };
 }
 
-const authCtrl = new AuthController()
-module.exports = authCtrl
+const authCtrl = new AuthController();
+module.exports = authCtrl;
--- src/services/mail.service.js
+++ src/services/mail.service.js
@@ -6,11 +6,11 @@ class MailServices {
   constructor() {
     try {
       this.transport = nodemailer.createTransport({
-        host: SMTP_HOST,
-        port: SMTP_PORT,
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
         auth: {
-          user: SMTP_USERNAME,
-          pass: SMTP_PASSWORD,
+          user: process.env.SMTP_USERNAME,
+          pass: process.env.SMTP_PASSWORD,
         },
       });
     } catch (exception) {
@@ -27,6 +27,8 @@ class MailServices {
         html: message,
         attachments: attachments,
       });
+      console.log(mailStatus);
+      return mailStatus;
     } catch (exception) {
       console.log(exception);
       throw new Error("Error sending mail");
